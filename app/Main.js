2d5b26f86610308abac983e44babef3dfa2acb5f
Hayden <hayden@aitch.co>
2020-05-17 18:55:24 +1000

    First commit for react edition

---
+++ app/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Components
+import Header from "./components/Header";
+import HomeGuest from './components/HomeGuest';
+import Footer from './components/Footer';
+
+function Main() {
+    return(
+        <>    
+            <Header />
+            <HomeGuest />
+            <Footer />        
+        </>
+    )
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"));
