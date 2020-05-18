273a187a876b4e03ae2bcf3326173c2826713f29
Hayden <hayden@aitch.co>
2020-05-18 22:29:22 +1000

    connected front-end to back-end and added flash messages

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Page from './Page';
+import Axios from 'axios';
 
 function HomeGuest() {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            await Axios.post('/register', {
+                username,
+                email,
+                password
+            })
+        } catch(error) {
+            console.log("There was an error");
+        }
+    }
+
     return(
-        <div className="container py-md-5">
+        <Page title="Welcome" wide={true}>
             <div className="row align-items-center">
                 <div className="col-lg-7 py-3 py-md-5">
                     <h1 className="display-3">Remember Writing?</h1>
@@ -11,35 +30,35 @@ function HomeGuest() {
                         is the key to enjoying the internet again.</p>
                 </div>
                 <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="form-group">
                             <label htmlFor="username-register" className="text-muted mb-1">
                                 <small>Username</small>
                             </label>
-                            <input id="username-register" name="username" className="form-control" type="text"
+                            <input onChange={event => setUsername(event.target.value)} id="username-register" name="username" className="form-control" type="text"
                                 placeholder="Pick a username" autoComplete="off" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="email-register" className="text-muted mb-1">
                                 <small>Email</small>
                             </label>
-                            <input id="email-register" name="email" className="form-control" type="text"
+                            <input onChange={event => setEmail(event.target.value)} id="email-register" name="email" className="form-control" type="text"
                                 placeholder="you@example.com" autoComplete="off" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="password-register" className="text-muted mb-1">
                                 <small>Password</small>
                             </label>
-                            <input id="password-register" name="password" className="form-control" type="password"
+                            <input onChange={event => setPassword(event.target.value)} id="password-register" name="password" className="form-control" type="password"
                                 placeholder="Create a password" />
                         </div>
                         <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
-                            Sign up for ComplexApp
+                            Sign up for My Journal
                         </button>
                     </form>
                 </div>
             </div>
-        </div>
+        </Page>
     )
 }
 
