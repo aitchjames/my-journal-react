7eeb5a7684785558b297aa4e212fafa67c0cc63b
Hayden <hayden@aitch.co>
2020-05-26 10:08:48 +1000

    user can delete posts and only author can see edit and delete actions

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from "react"
-import { useParams, Link } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react"
+import { useParams, Link, withRouter } from "react-router-dom";
 import Axios from "axios";
 import ReactMarkdown from 'react-markdown';
 import ReactTooltip from 'react-tooltip';
 import Page from './Page';
 import LoadingDotsIcon from "./LoadingDotsIcon";
 import NotFound from "./NotFound";
+import StateContext from "../StateContext";
+import DispatchContext from "../DispatchContext";
 
-function ViewSinglePost() {
+function ViewSinglePost(props) {
+    const appState = useContext(StateContext);
+    const appDispatch  = useContext(DispatchContext);
+
     const { id } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [post, setPost] = useState();
@@ -39,16 +44,43 @@ function ViewSinglePost() {
     const date = new Date(post.createdDate);
     const dateFormatted = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 
+    function isAuthor() {
+        if (appState.loggedIn) {
+            return appState.user.username == post.author.username
+        }
+        return false
+    }
+
+    async function deleteHandler() {
+        const areYouSure = window.confirm("Do you want to delete?");
+        if (areYouSure) {
+            try {
+                const response = await Axios.delete(`/post/${id}`, {data: {token: appState.user.token}});
+                if (response.data == "Success") {
+                    // Display flash message
+                    appDispatch({type: "flashMessage", value: "Post was successfully deleted"})
+                    // Redirect to user profile
+                    props.history.push(`/profile/${appState.user.username}`)
+                }
+            } catch(error) {
+                console.log("There was a problem")
+            }
+        }
+    }
+
     return (
         <Page title={post.title}>
             <div className="d-flex justify-content-between">
             <h2>{post.title}</h2>
-            <span className="pt-2">
-            <Link to={`/post/${post._id}/edit`} data-tip="Edit" data-for="edit" className="text-primary mr-2"><i className="fas fa-edit"></i></Link>
-            <ReactTooltip id="edit" class="custom-tooltip" />{" "}
-            <a data-tip="Delete" data-for="delete" className="delete-post-button text-danger"><i className="fas fa-trash"></i></a>
-            <ReactTooltip id="delete" class="custom-tooltip" />
-            </span>
+            {isAuthor() && (
+                <span className="pt-2">
+                    <Link to={`/post/${post._id}/edit`} data-tip="Edit" data-for="edit" className="text-primary mr-2"><i className="fas fa-edit"></i></Link>
+                    <ReactTooltip id="edit" class="custom-tooltip" />{" "}
+                    <a onClick={deleteHandler} data-tip="Delete" data-for="delete" className="delete-post-button text-danger"><i className="fas fa-trash"></i></a>
+                    <ReactTooltip id="delete" class="custom-tooltip" />
+                </span>
+            )}
+            
         </div>
 
         <p className="text-muted small mb-4">
@@ -65,4 +97,4 @@ function ViewSinglePost() {
     )
 }
 
-export default ViewSinglePost
+export default withRouter(ViewSinglePost);
