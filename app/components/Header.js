99165a5150b4b1ae9b263edf72667eb665a9f668
Hayden <hayden@aitch.co>
2020-05-29 15:06:39 +1000

    added files to connect to netlify

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,6 +7,8 @@ import StateContext from '../StateContext';
 
 function Header(props) {
     const appState = useContext(StateContext)
+    const headerContent = appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />
+    
     return(
         <header className="header-bar bg-primary mb-3">
             <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -15,7 +17,7 @@ function Header(props) {
                     My Journal
                     </Link>
                 </h4>
-                {appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
+                {!props.staticEmpty ? headerContent : ""}
             </div>
         </header>
     )
